c3309be2a69c2e4d55ac32e05e2b1c7a7a9928f5
akshanshsingh00 <akshanshsingh00@gmail.com>
2024-07-02 16:52:59 +0530

    Starting backend

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -2,11 +2,22 @@ import React from "react";
 import UserInfo from "./UserInfo";
 import ChatList from "./ChatList";
 
-const List = ({ className }) => {
+const List = ({
+  className,
+  modalIsOpen,
+  setModalIsOpen,
+  openModal,
+  closeModal,
+}) => {
   return (
     <div className={`${className} flex flex-col flex-1 h-[100%]`}>
       <UserInfo />
-      <ChatList />
+      <ChatList
+        modalIsOpen={modalIsOpen}
+        setModalIsOpen={setModalIsOpen}
+        openModal={openModal}
+        closeModal={closeModal}
+      />
     </div>
   );
 };
---
+++ src/lib/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCBGvfq9xLMTNuD8zAhefc_CQ3eGN5CnIQ",
+  authDomain: "react-chat-site-3cc86.firebaseapp.com",
+  projectId: "react-chat-site-3cc86",
+  storageBucket: "react-chat-site-3cc86.appspot.com",
+  messagingSenderId: "398931737007",
+  appId: "1:398931737007:web:ee4596b761126d7523975a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
